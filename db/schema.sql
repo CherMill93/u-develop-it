8930600ed7e4a418cff77190b1aaca4838fb645b
CherMill <cmille2016@gmail.com>
2022-01-17 14:19:01 -0700

    12.5.3

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS parties;
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS voters;
+drop table if exists votes;
 
 CREATE TABLE parties (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,4 +24,14 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+create table votes (
+  id integer AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id integer not null,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT uc_voter unique (voter_id),
+  constraint fk_voter FOREIGN KEY (voter_id) references voters(id) on delete cascade,
+  constraint fk_candidate foreign key (candidate_id) references candidates(id) on delete cascade
 );
